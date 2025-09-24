16bbe8db4b7d3a7740991ceaa04d556fd1f3af72
Al Faridul Karim <127551009+Alfa5nm@users.noreply.github.com>
2025-09-25 04:18:48 +0600

    Document starting Docker Desktop before running commands

---
+++ delegates.rb
@@ -0,0 +1,28 @@
+require 'logger'
+
+module Cantaloupe
+  class Delegates
+    NASA_KAGUYA_S3 = {
+      'kaguya-band1' => 'https://nasa-lunar-data.s3.us-west-2.amazonaws.com/kaguya/mi/global_mosaic_60m/Kaguya_MI_Band1_60m.tif',
+      'kaguya-band2' => 'https://nasa-lunar-data.s3.us-west-2.amazonaws.com/kaguya/mi/global_mosaic_60m/Kaguya_MI_Band2_60m.tif'
+    }.freeze
+
+    def initialize(context = {})
+      @context = context
+      @logger = Logger.new($stdout)
+    end
+
+    # Map the short IIIF identifier to the absolute URL in the NASA S3 bucket
+    def source_resource_identifier
+      identifier = @context['identifier']
+      resolved = NASA_KAGUYA_S3[identifier]
+
+      unless resolved
+        @logger.warn("No mapping found for identifier '#{identifier}'")
+      end
+
+      resolved
+    end
+
+  end
+end
